5e7ab5bb00ebbc5c946260ab2c1066c8cab47972
aryelmahdi <elprab17@gmail.com>
2023-01-10 08:21:01 +0000

    commit 2

--- edit.php
+++ edit.php
@@ -1,6 +1,7 @@
 <?php require "Config/database.php" ?>
 <!DOCTYPE html>
 <html lang="en">
+	<!--ini tambahan-->
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
